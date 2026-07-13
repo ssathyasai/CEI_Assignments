a61666af8585d0437c09901ad136f1c276354e13
ssathyasai <sathyasai1357@gmail.com>
2026-07-13 13:38:45 +0530

    Updated requirements.txt

--- week7_Sathyasai/utils/document_processor.py
+++ week7_Sathyasai/utils/document_processor.py
@@ -8,7 +8,7 @@ import os
 from pathlib import Path
 from typing import List, Any
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 class DocumentProcessor:
