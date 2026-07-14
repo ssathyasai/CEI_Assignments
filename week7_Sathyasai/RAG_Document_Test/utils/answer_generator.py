9e50393c2128a0a4b75c74d54edadf41bb65ec31
ssathyasai <sathyasai1357@gmail.com>
2026-07-14 14:13:05 +0530

    Fix answer_generator imports - use langchain.chains.retrieval

--- week7_Sathyasai/RAG_Document_Test/utils/answer_generator.py
+++ week7_Sathyasai/RAG_Document_Test/utils/answer_generator.py
@@ -1,7 +1,7 @@
 """Answer Generation Module - Uses Groq LLM"""
 from typing import Dict, Any
 from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
